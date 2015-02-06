a2c77dd327e6eebef20dc0750b1a56e10791f86b
Bryan Dixon <javawolfpack@gmail.com>
2015-02-05 19:22:32 -0800

    Lecture 6 added
    Signed-off-by: Bryan Dixon <javawolfpack@gmail.com>

---
+++ Lecture6/app/src/main/java/examples/csci567/lecture6/MainActivity.java
@@ -0,0 +1,77 @@
+package examples.csci567.lecture6;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private AlarmReceiver alarm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        alarm = new AlarmReceiver();
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch(id){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_startalarm:
+                alarm.setAlarm(this);
+                Toast.makeText(this, "Alarm Started", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_stopalarm:
+                alarm.cancelAlarm(this);
+                Toast.makeText(this, "Alarm Stopped", Toast.LENGTH_LONG).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
